c9546bb2909f6af78ead64a21d3ea8526b250ba8
vigneshsg190 <vigneshsg440@gmail.com>
2025-06-17 14:51:34 +0530

    Initial commit

---
+++ src/components/contact.jsx
@@ -0,0 +1,59 @@
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+import Lottie from 'lottie-react';
+import mailAnimation from '../assets/Animation - 1750004829267.json';
+import githubAnimation from '../assets/Animation - 1750010113879.json';
+import linkedinAnimation from '../assets/Animation - 1750010311571.json';
+
+
+// downloaded from lottiefiles.com
+                    const items = ['a','d'];
+
+
+function Contact() {
+    return (
+        <>
+<ul>{items.map((item,i) => <li> {i}</li>)}</ul>
+
+            <div className=" w-9/12  mx-auto py-12">
+                <p className="text-2xl">Contact</p>
+                <div className="flex gap-6 mt-4 items-center">
+                    <Lottie
+                        animationData={mailAnimation}
+                        loop={true}
+                        autoplay={true} className="w-15 h-14"
+                    />
+                    <span>vigneshsg@gmail.com</span>
+                    
+                </div>
+                <div className="flex gap-6 mt-4 items-center">
+                    <Lottie
+                        animationData={githubAnimation}
+                        loop={true}
+                        autoplay={true} className="w-15 h-14"
+
+                    />
+                    <span>github.com/vigneshsg190</span>
+                    
+                </div>
+                <div className="flex gap-6 mt-4 items-center">
+                    <Lottie
+                        animationData={linkedinAnimation}
+                        loop={true}
+                        autoplay={true} className="w-15 h-14"
+
+                    />
+                    <span>vigneshsg@gmail.com</span>
+                    
+                </div>
+                
+            </div>
+
+
+        </>
+    )
+}
+
+export default Contact;
+
---
+++ src/components/header.jsx
@@ -0,0 +1,33 @@
+
+function Header() {
+  return (
+    <nav className="bg-grey border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Portfolio</span>
+        </a>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View My Work</button>
+      
+  </div>
+  <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border text-white md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  ">
+      <li>
+        <a href="#" class="block py-2 px-3 md:p-0 text-white rounded hover:text-gray-900  md:hover:bg-transparent dark:hover:bg-gray-700 md:dark:hover:bg-transparent" aria-current="page">About</a>
+      </li>
+      <li>
+        <a href="#" className="block py-2 px-3 md:p-0 text-white rounded hover:text-gray-900  md:hover:bg-transparent dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Projects</a>
+      </li>
+      <li>
+        <a href="#" className="block py-2 px-3 md:p-0 text-white rounded hover:text-gray-900  md:hover:bg-transparent dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Contact</a>
+      </li>
+      
+    </ul>
+  </div>
+  </div>
+</nav>
+  );
+}
+
+export default Header;
